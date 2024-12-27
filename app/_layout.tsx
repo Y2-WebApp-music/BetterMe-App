dc3595ac03a6833b7b35c54aa631d03f3c185a2f
guynut <kunguy.159@gmail.com>
2024-12-27 08:20:57 +0700

    🚀 StartProject

---
+++ app/_layout.tsx
@@ -0,0 +1,48 @@
+import {
+  NotoSans_100Thin,
+  NotoSans_200ExtraLight,
+  NotoSans_300Light,
+  NotoSans_400Regular,
+  NotoSans_500Medium,
+  NotoSans_600SemiBold,
+  NotoSans_700Bold,
+  NotoSans_800ExtraBold,
+  NotoSans_900Black,
+} from "@expo-google-fonts/noto-sans";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import React, { useEffect } from "react";
+import "../global.css";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    NotoSans_100Thin,
+    NotoSans_200ExtraLight,
+    NotoSans_300Light,
+    NotoSans_400Regular,
+    NotoSans_500Medium,
+    NotoSans_600SemiBold,
+    NotoSans_700Bold,
+    NotoSans_800ExtraBold,
+    NotoSans_900Black,
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
