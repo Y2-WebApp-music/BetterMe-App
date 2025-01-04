52a261286b74840ae4e3aa05fc5cc5d902a55af8
guynut <kunguy.159@gmail.com>
2025-01-04 20:58:35 +0700

    🚧 Goal Page #1

--- components/goal/goalCreateCard.tsx
+++ components/goal/goalCreateCard.tsx
@@ -24,8 +24,8 @@ const GoalCreateCard = ({goal_id, goal_name, start_date, end_date, length_task, 
 
   return (
     <TouchableOpacity onPress={()=>{router.push(`/home/goal/create/${goal_id}`)}} className='h-32 p-4 w-full justify-center rounded-normal border border-gray bg-white flex-row'>
-      <View style={{width:'84%', height:'94%'}} className='flex justify-center items-start'>
-        <View style={{width:'100%', height:'66%'}} className='justify-center'>
+      <View style={{width:'89%', height:'100%'}} className='flex justify-center items-start'>
+        <View style={{width:'100%', height:'70%'}} className='justify-center'>
           <Text
             style={{overflow: 'hidden',}}
             numberOfLines={2}
@@ -38,7 +38,7 @@ const GoalCreateCard = ({goal_id, goal_name, start_date, end_date, length_task, 
         <Text className='text-detail text-subText font-noto'>create by : {create_by}</Text>
         <Text className='text-detail text-subText font-noto'>{length_task} Task duration : {duration}</Text>
       </View>
-      <View style={{width:'10%', height:'94%'}} className=' justify-center items-end'>
+      <View style={{width:'10%', height:'100%'}} className=' justify-center items-end'>
         <AddIcon color={'#1C60DE'}/>
       </View>
     </TouchableOpacity>
