24f0498de8d9e16d96a5670c0d2ead7b46526a83
Ariez <kritamet.cho@gmail.com>
2025-01-07 00:11:25 +0700

    ✏️ change attribute name

--- types/goal.ts
+++ types/goal.ts
@@ -1,20 +1,20 @@
 type goal = {
-  goal_id:string
-  goal_name:string
-  length_task:number
+  goal_id: string
+  goal_name: string
+  total_task: number
 }
 export type homeGoalCardProp = goal & {
-  complete_task:number
-  end_date:Date
+  complete_task: number
+  end_date: Date
 }
 export type calendarGoalCardProp = goal & {
-  complete_task:number
+  complete_task: number
 }
 
 export type GoalCreateCardProp = goal & {
-  start_date:Date
-  create_by:string
-  end_date:Date
+  start_date: Date
+  create_by: string
+  end_date: Date
 }
 
 export type Task = {
@@ -23,7 +23,7 @@ export type Task = {
 };
 
 export type GoalData = goal & {
-  end_date:Date
+  end_date: Date
   description: string;
   start_date: Date;
   task: Task[];
@@ -31,52 +31,52 @@ export type GoalData = goal & {
 };
 
 
-export const goalDataDummy:homeGoalCardProp[] = [
+export const goalDataDummy: homeGoalCardProp[] = [
   {
-    goal_id:'1',
-    goal_name:'Title 1 Line test text inline style Second Line First Line test text inline style Second Line First Line test text inline style Second Line',
-    end_date:new Date(),
-    length_task:12,
-    complete_task:12,
+    goal_id: '1',
+    goal_name: 'Title 1 Line test text inline style Second Line First Line test text inline style Second Line First Line test text inline style Second Line',
+    end_date: new Date(),
+    total_task: 12,
+    complete_task: 12,
   },
   {
-    goal_id:'2',
-    goal_name:'Title 2',
-    end_date:new Date(new Date().setDate(new Date().getDate() + 45)),
-    length_task:6,
-    complete_task:2,
+    goal_id: '2',
+    goal_name: 'Title 2',
+    end_date: new Date(new Date().setDate(new Date().getDate() + 45)),
+    total_task: 6,
+    complete_task: 2,
   },
   {
-    goal_id:'3',
-    goal_name:'Title 3',
-    end_date:new Date(new Date().setDate(new Date().getDate() + 10)),
-    length_task:6,
-    complete_task:3,
+    goal_id: '3',
+    goal_name: 'Title 3',
+    end_date: new Date(new Date().setDate(new Date().getDate() + 10)),
+    total_task: 6,
+    complete_task: 3,
   },
 ]
-export const goalCreateDataDummy:GoalCreateCardProp[] = [
+export const goalCreateDataDummy: GoalCreateCardProp[] = [
   {
-    goal_id:'1',
-    goal_name:'Title 1 Line test text inline style Second Line First Line test text inline style Second Line First Line test text inline style Second Line',
-    start_date:new Date(),
-    end_date:new Date(new Date().setDate(new Date().getDate() + 10)),
-    length_task:12,
-    create_by:'system'
+    goal_id: '1',
+    goal_name: 'Title 1 Line test text inline style Second Line First Line test text inline style Second Line First Line test text inline style Second Line',
+    start_date: new Date(),
+    end_date: new Date(new Date().setDate(new Date().getDate() + 10)),
+    total_task: 12,
+    create_by: 'System'
   },
   {
-    goal_id:'2',
-    goal_name:'Title 2',
-    start_date:new Date(new Date().setDate(new Date().getDate() + 45)),
-    end_date:new Date(new Date().setDate(new Date().getDate() + 90)),
-    length_task:6,
-    create_by:'guy nut'
+    goal_id: '2',
+    goal_name: 'Title 2',
+    start_date: new Date(new Date().setDate(new Date().getDate() + 45)),
+    end_date: new Date(new Date().setDate(new Date().getDate() + 90)),
+    total_task: 6,
+    create_by: 'guy nut'
   },
   {
-    goal_id:'3',
-    goal_name:'Title 3',
-    start_date:new Date(new Date().setDate(new Date().getDate() + 6)),
-    end_date:new Date(new Date().setDate(new Date().getDate() + 80)),
-    length_task:6,
-    create_by:'manotham'
+    goal_id: '3',
+    goal_name: 'Title 3',
+    start_date: new Date(new Date().setDate(new Date().getDate() + 6)),
+    end_date: new Date(new Date().setDate(new Date().getDate() + 80)),
+    total_task: 6,
+    create_by: 'manotham'
   },
 ]
