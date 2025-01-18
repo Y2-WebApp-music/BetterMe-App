c818c3be4f404668f9b9c606c09266acaf1e8b0a
guynut <kunguy.159@gmail.com>
2025-01-18 16:38:22 +0700

    🚧 addMealPage#2

---
+++ components/NumberInput.tsx
@@ -0,0 +1,61 @@
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { Feather } from '@expo/vector-icons'
+
+type TextInputProp = {
+  name:string
+  value:number
+  handleChange:(text:number) => void
+}
+
+const NumberInput:React.FC<TextInputProp> = ({name, value, handleChange}) => {
+
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const borderColor = value || isFocused ? '#1C60DE' : '#E8E8E8'
+
+  return (
+    <View className='w-full mt-2'
+      style={{marginTop: 10}}
+    >
+      <Text className='text-subText text-detail'>{name}</Text>
+      <View
+        className='w-full flex justify-center border border-gray focus:border-primary rounded-normal'
+        style={[
+          styles.inputContainer,
+          { borderColor },
+        ]}
+      >
+        <TextInput
+          className='flex-1 text-primary text-heading2 font-noto'
+          style={{
+            height: 40,
+            width:"94%",
+            textAlignVertical: "center",
+          }}
+          value={value.toString()}
+          placeholder={name}
+          placeholderTextColor="#CFCFCF"
+          onChangeText={(text) => handleChange(Number(text) || 0)}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderRadius: 12,
+    paddingLeft: 4,
+    paddingRight: 4,
+    borderWidth: 1,
+  },
+})
+
+export default NumberInput
