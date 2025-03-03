a34c7e8aee97fb054a92b3a1ec3bde9935331ae8
guynut <kunguy.159@gmail.com>
2025-03-03 16:58:17 +0700

    🌱PostCreate

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -59,6 +59,7 @@ export default function RootLayout() {
             <Stack.Screen name="(auth)" options={{ headerShown: false }} />
             <Stack.Screen name="welcome" options={{ headerShown: false }} />
             <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="postCreate" options={{ headerShown: false }} />
             <Stack.Screen name="oauthredirect" options={{ headerShown: false }} />
           </Stack>
         </ProtectedLogin>
