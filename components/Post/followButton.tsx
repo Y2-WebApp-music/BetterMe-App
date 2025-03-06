868fabf350a31d438b5e18e498ebfa967bea7731
guynut <kunguy.159@gmail.com>
2025-03-07 02:00:43 +0700

    ✅ Follow Btn

--- components/Post/followButton.tsx
+++ components/Post/followButton.tsx
@@ -1,28 +1,93 @@
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AddIcon } from '../../constants/icon';
+import { useTheme } from '../../context/themeContext';
+import { useAuth } from '../../context/authContext';
+import axios from 'axios';
+import { SERVER_URL } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const FollowButton = () => {
-  const [following, setFollowing] = useState(false);
+const FollowButton = ({userPostID}:{userPostID:string}) => {
 
-  const handleFollow = () => {
-    setFollowing(!following);
+  const { user, userFollow, setUserFollow } = useAuth()
+  const { colors } = useTheme();
+
+  const [isFollowing, setIsFollowing] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (userFollow && userPostID) {
+      setIsFollowing(userFollow.following.includes(userPostID));
+    }
+  }, [userFollow, userPostID]);
+
+  const followUpdate = async () => {
+    if (!user || loading) return;
+
+    setLoading(true);
+    
+    try {
+      const response = await axios.put(`${SERVER_URL}/community/user/follow?user_id=${userPostID}&follower_id=${user._id}`);
+      const data = response.data;
+      console.log('data.message ', data.message);
+
+      setIsFollowing(!isFollowing);
+    } catch (error: any) {
+      console.error('Follow Error: ', error);
+    } finally {
+      setLoading(false);
+    }
   };
 
+  const getFollowData = async (_id:string) => {
+    try {
+      await AsyncStorage.removeItem('@follow');
+
+      const response = await axios.get(`${SERVER_URL}/user/follow/${_id}`);
+      const res = response.data
+
+      if ( res.message === "User not found") { return console.log('User not found in follow');}
+
+      console.log('follow res ',res);
+      setUserFollow({
+        follower: res.follower,
+        following: res.following,
+      })
+
+      await AsyncStorage.setItem('@follow', JSON.stringify(res));
+    } catch (error) {
+      console.error('followUser get failed', error);
+    }
+  }
+
+  const handleFollow = async () => {
+    await followUpdate().finally(()=>{user && getFollowData(user._id)})
+  };
+  
+
   return (
-    <TouchableOpacity 
-      onPress={handleFollow} 
-      className={`flex-row gap-2 p-1 px-1 justify-center items-center rounded-full w-auto ${following ? 'bg-gray' : 'bg-primary'}`}
-      style={{paddingHorizontal: 10,  alignSelf: 'flex-start', }}
+    <TouchableOpacity
+      onPress={handleFollow}
+      className={`flex-row gap-1 p-1 px-1 justify-center items-center rounded-full w-auto`}
+      style={{
+        paddingHorizontal: 10,
+        alignSelf: 'flex-start',
+        backgroundColor: isFollowing ? colors.gray : colors.primary,
+        paddingLeft: isFollowing ? 12 : 14,
+      }}
+      disabled={loading}
     >
-      <Text className={`text-body font-notoMedium ${following ? 'text-subText' : 'text-white'}`}>
-        {following ? 'following' : 'follow'}
+      <Text style={{ color: isFollowing ? colors.subText : '#fff' }} className={`text-body font-notoMedium`}>
+        {loading ? 'Loading...' : isFollowing ? 'Following' : 'Follow'}
       </Text>
-      {!following && (
-        <AddIcon width={24} height={24} color={'white'}/>
+      {!isFollowing && !loading && (
+        <AddIcon width={24} height={24} color={'white'} />
+      )}
+      {loading && (
+        <Text style={{ color: '#fff' }}>...</Text>
       )}
     </TouchableOpacity>
-  )
+  );
 }
 
 export default FollowButton;
--- context/authContext.tsx
+++ context/authContext.tsx
@@ -5,12 +5,14 @@ import { router } from 'expo-router';
 import { GoogleAuthProvider, IdTokenResult, onAuthStateChanged, signInWithCredential, signOut, User, UserInfo, UserMetadata } from 'firebase/auth';
 import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react';
 import { auth } from '../components/auth/firebaseConfig';
-import { UserData } from '../types/user';
+import { UserData, UserFollow } from '../types/user';
 import LoadingBubble from '../components/auth/Loading';
 
 type AuthContextType = {
   user: UserData | null;
   setUser: React.Dispatch<React.SetStateAction<UserData | null>>;
+  userFollow: UserFollow | null;
+  setUserFollow: React.Dispatch<React.SetStateAction<UserFollow | null>>;
   loginWithGoogle: (id_token: string) => Promise<void>;
   signOut: () => Promise<void>;
 };
@@ -27,6 +29,7 @@ export const useAuth = () => {
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<UserData | null>(null);
+  const [userFollow, setUserFollow] = useState<UserFollow | null>(null);
 
   // Check local user and Firebase auth state
   const checkLocalUser = async () => {
@@ -36,6 +39,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
       // console.log(localUser);
       if (localUser) {
         setUser(localUser);
+        followUser(localUser._id)
       }
     } catch (e) {
       console.error('Error fetching user from AsyncStorage', e);
@@ -61,6 +65,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
           console.warn('User not found Loading.....');
           return <LoadingBubble />;
         }
+
         
         else {
           console.warn('.....Have User in Data base.....');
@@ -80,6 +85,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
           // console.log(' :=========== userData ===========: \n',extendedUser);
           setUser(extendedUser);
 
+          followUser(_id)
           await AsyncStorage.setItem('@user', JSON.stringify(extendedUser));
         }
       } catch (error) {
@@ -121,9 +127,28 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     }
   };
 
+  const followUser = async (_id:string) => {
+    try {
+      const response = await axios.get(`${SERVER_URL}/user/follow/${_id}`);
+      const res = response.data
+
+      if ( res.message === "User not found") { return console.log('User not found in follow');}
+
+      console.log('follow res ',res);
+      setUserFollow({
+        follower: res.follower,
+        following: res.following,
+      })
+
+      await AsyncStorage.setItem('@follow', JSON.stringify(res));
+    } catch (error) {
+      console.error('followUser get failed', error);
+    }
+  }
+
 
   return (
-    <AuthContext.Provider value={{ user, setUser, loginWithGoogle, signOut: handleSignOut }}>
+    <AuthContext.Provider value={{ user, setUser, userFollow, setUserFollow, loginWithGoogle, signOut: handleSignOut }}>
       {children}
     </AuthContext.Provider>
   );
